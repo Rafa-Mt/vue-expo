bbcc8cbd5fd3f75c1868d70f8be3d989a7c56a43
JoseCarrilloURU <jose.30739823@uru.edu>
2024-02-08 01:49:19 -0400

    Parte de José Carrillo

--- src/router/slides.js
+++ src/router/slides.js
@@ -1,7 +1,19 @@
 import Reactividad from "../components/slides/Reactividad/Reactividad.mdx";
 import Directivas from "../components/slides/Directivas/Directivas.mdx";
+import APIs from "../components/slides/APIs/APIs.mdx";
+import TemplateSyntax from "../components/slides/TemplateSyntax/TemplateSyntax.mdx";
 
 export default [
+  {
+    name: "APIs",
+    path: "/APIs",
+    component: APIs,
+  },
+  {
+    name: "TemplateSyntax",
+    path: "/TemplateSyntax",
+    component: TemplateSyntax,
+  },
   {
     name: "Reactividad",
     path: "/reactividad",
