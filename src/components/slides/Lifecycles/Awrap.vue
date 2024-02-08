503e4f7340671f7c951f70b9c98393069bf23d15
Atla004 <andrese.g.v.13579@gmail.com>
2024-02-07 22:23:49 -0400

    Añadido Lifecycles o algo asi

---
+++ src/components/slides/Lifecycles/Awrap.vue
@@ -0,0 +1,62 @@
+<template>
+  <div> 
+    <p>El botón ha sido presionado {{ count }} veces.</p>   
+    <button id="count" @click="count++">{{ count }}</button>
+  </div>
+</template>
+  
+<script setup>
+  import { ref, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted, onBeforeMount } from 'vue';
+  
+  // Variables reactivas
+  const count = ref(0);
+  
+  // Hooks
+
+  onBeforeMount(() => {
+    console.log('Antes de montar el componente(onBeforeMount)');
+  });
+
+  onMounted(() => {
+    console.log('El componente ha sido montado (onMounted)');
+  });
+  
+  onBeforeUpdate(() => {
+    console.log('Antes de actualizar el componente(onBeforeUpdate)');
+  });
+  
+  onUpdated(() => {
+    console.log('El componente ha sido actualizado(onUpdated)');
+  });
+  
+  onBeforeUnmount(() => {
+    console.log('Antes de desmontar el componente(onBeforeUnmount)');
+  });
+  
+  onUnmounted(() => {
+    console.log('El componente ha sido desmontado(onUnmounted)');
+  });
+
+</script>
+  
+<style scoped>
+  button {
+    border-radius: 8px;
+    border: 1px solid transparent;
+    padding: 0.6em 1.2em;
+    font-size: 1em;
+    font-weight: 500;
+    font-family: inherit;
+    background-color: #205a33;
+    cursor: pointer;
+    transition: border-color 0.25s;
+  }
+  button:hover {
+    border-color: #646cff;
+  }
+  button:focus,
+  button:focus-visible {
+    outline: 4px auto -webkit-focus-ring-color;
+  }
+</style>
+  
--- src/router/slides.js
+++ src/router/slides.js
@@ -12,4 +12,5 @@ export default [
     path: "/directivas",
     component: Directivas,
   },
+
 ];
