4ac1328a257c0a304c5a9f41b12adf692879b261
JoseCarrilloURU <jose.30739823@uru.edu>
2024-02-14 23:39:51 -0400

    Segunda Parte

---
+++ src/components/slides/MoreBuiltInComponents/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Modal from './Modal.vue'
+import { ref } from 'vue'
+
+const showModal = ref(false)
+</script>
+
+<template>
+  <button id="show-modal" @click="showModal = true">Click for Modal!</button>
+
+ 
+    <!-- use the modal component, pass in the prop -->
+    <modal :show="showModal" @close="showModal = false">
+      <template #header>
+        <h3>¡Esto es el Modal!</h3>
+      </template>
+    </modal>
+  
+</template>
--- src/router/slides.js
+++ src/router/slides.js
@@ -2,6 +2,8 @@ import Reactividad from "../components/slides/Reactividad/Reactividad.mdx";
 import Directivas from "../components/slides/Directivas/Directivas.mdx";
 import APIs from "../components/slides/APIs/APIs.mdx";
 import TemplateSyntax from "../components/slides/TemplateSyntax/TemplateSyntax.mdx";
+import Transitions from "../components/slides/BuiltInComponents/Transition&TransitionGroup.mdx";
+import KeepAlive from "../components/slides/MoreBuiltInComponents/KeepAlive&Teleport.mdx";
 
 export default [
   {
@@ -23,6 +25,16 @@ export default [
     name: "Directivas",
     path: "/directivas",
     component: Directivas,
+  },
+  {
+    name: "BuiltInComponents",
+    path: "/Transition&TransitionGroup",
+    component: Transitions,
+  },
+  {
+    name: "MoreBuiltInComponents",
+    path: "/MoreBuiltInComponents",
+    component: KeepAlive,
   },
 
 ];
